896c3cdd3115aa207e705f68d4f97e2eded099d1
Shuojun Chen <shuojunchen@gmail.com>
2025-02-13 11:42:51 -0800

    move the readFile inside the writeFile

--- week6/app.js
+++ week6/app.js
@@ -5,5 +5,13 @@ fs.writeFile("data.txt", "This is a message for you", (err) => {
       return console.error('Error writing file:', err);
     } else {
       console.log('File written successfully.');
+      fs.readFile("data.txt", "utf8", (err, data) => {
+        if (err) {
+          return console.error('Error reading file:', err);
+        } else {
+          console.log('File contents:', data);
+        }
+    });
     }
 });
+
