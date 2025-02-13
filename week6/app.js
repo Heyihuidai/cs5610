c921a77af3c8e9aa60e273b1a79778495af6a60d
Shuojun Chen <shuojunchen@gmail.com>
2025-02-13 12:20:48 -0800

    let localhost run

--- week6/app.js
+++ week6/app.js
@@ -22,7 +22,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello and welcome to my site!');
 });
 
 const port = 3000;
