a7a2c9fb3b088e8a0cfebd68aa938f24102eb832
Shuojun Chen <shuojunchen@gmail.com>
2025-02-20 11:12:18 -0800

    add static html page

--- week6/app.js
+++ week6/app.js
@@ -21,6 +21,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Hello and welcome to my site!');
 });
